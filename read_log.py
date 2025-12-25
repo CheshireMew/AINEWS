60bc1f767efe1f376c1a06273437069f49d55b15
Web3小柴郡 <Dylan.Z.Xin@outlook.com>
2025-12-26 00:36:25 +0800

    chore: 清理测试和调试文件，添加项目技术文档

--- read_log.py
+++
@@ -1,3 +0,0 @@
-with open('scraper_output.log', 'r', encoding='utf-16le') as f:
-    content = f.read()
-    print(content)
--- simple_test.py
+++
@@ -1,8 +0,0 @@
-import re
-
-# 模拟包含多个连续换行的内容
-test = "段落1\n\n\n\n段落2"
-
-print("原始:", repr(test))
-print("方案1 结果:", repr(re.sub(r'\n\s*\n+', '\n', test)))
-print("方案2 结果:", repr(re.sub(r'\n+', '\n', test)))
